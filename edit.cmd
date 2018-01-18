e9bef9700651cd798c3d6a934caba64ba39b73e6
Chihiro Sakai <sakaic2003@gmail.com>
2018-01-18 16:56:19 +0900

    Apply .gitignore

--- edit.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-python -uc "print('@echo off')" > temp.cmd
-echo "%PROGRAMFILES%\VSCode\Code.exe" %~dp0 >> temp.cmd
-python -uc "print('exit')" >> temp.cmd
-start /B temp.cmd
-ping localhost -n 1 > nul
-del temp.cmd
-exit
